d8ab09c22923a2b868a29c2556699c8c0c62b7cb
aleenabiju123 <150226690+aleenabiju123@users.noreply.github.com>
2024-05-21 14:45:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,88 @@
+// console.log("Hello World!!");
+// let a=1;
+// let b=2;
+// console.log(a+b);
+// {
+//     let a=10;
+// }
+//  let a=10;
+//  let b=20;
+//  console.log(a+b);
+//  console.log(a-b);
+//  console.log(a*b);
+//  console.log(a/b);
+//  console.log(a%b);
+//      // postfix increment);
+// let a=1;
+// let b=a++;
+// console.log(a);
+// console.log(b);
+ 
+//     //    prefix increment
+// let c=1;
+// let d=++c;
+// console.log(c);
+// console.log(d);
+    //  Object
+// let car={
+//     model:"Fortuner 4X2 ATde",
+//     colour:"black"
+// };
+// console.log(car);
+// console.log(car.colour);
+// console.log(car.model);
+
+//   let person={
+//     fname:"Aleena",
+//     address:{
+//         street:"abc",
+//         place:"xyx",
+//         pin:123
+//     },
+//     age:10
+//   };
+// //   console.log(person.address.place);
+// console.log(person.age);
+
+// var toothbrush=new Object();
+// toothbrush.brand="colgate";
+// toothbrush.colour="white";
+// // console.log(toothbrush);
+// console.log(toothbrush.brand)
+
+// Concatination
+//  let fname="Aleena";
+//  let lname="Biju";
+//  console.log(`${fname}  ${lname}`)
+
+//  let m=5;
+//  let n=6;
+//  console.log(`Sum is ${m+n}`)\
+//     //Array
+//       let fruits=["Apple","Mango","Orange",23] 
+//       console.log(fruits)
+//     // //   console.log(fruits[0]) 
+//     //   console.log(fruits.length)
+//     fruits.push("Grapes")
+//     // console.log(fruits)
+//    fruits[2]="Pineapple";
+//    fruits.unshift("Pear");
+//    fruits.pop()
+//    fruits.shift()
+//    console.log(fruits)
+    //  Array of objects
+     let bsc=[
+     {  
+        fname:"Aleena",
+        age:19,
+     },
+     {
+        fname:"devika",
+        age:25,
+     },
+     {
+        fname:"Anaina",
+        age:50,
+     },
+     ];
+     console.log(bsc[0].age)
